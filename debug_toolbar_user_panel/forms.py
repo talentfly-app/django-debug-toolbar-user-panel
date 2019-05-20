b41a9bd4f83060623f3a6880420598fc8b50cae4
Benjamin Port <benjamin.port@enioka.com>
2019-05-20 17:41:04 +0200

    allow to use custom USERNAME FIELD

--- debug_toolbar_user_panel/forms.py
+++ debug_toolbar_user_panel/forms.py
@@ -5,6 +5,8 @@ class UserForm(forms.Form):
     val = forms.CharField(label='User.{id,username,email}')
 
     def get_lookup(self):
+        from django.contrib.auth import get_user_model
+        username_field = get_user_model().USERNAME_FIELD
         val = self.cleaned_data['val']
 
         if '@' in val:
@@ -13,8 +15,8 @@ class UserForm(forms.Form):
         try:
             v = int(val)
             if len(val) >= 10:
-                return {'username': val}
+                return {username_field: val}
             else:
                 return {'pk': v}
         except:
-            return {'username': val}
+            return {username_field: val}
